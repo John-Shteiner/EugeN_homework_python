10169d35953a33eedd48059df8a4d1133dde1e54
Evgenii <jfaust866@gmail.com>
2026-03-02 16:43:59 +0700

    Lesson1

---
+++ lesson_1_task_2.py
@@ -0,0 +1,6 @@
+my_age = "39"
+my_age = "42"
+print(my_age)
+
+
+
---
+++ lesson_1_task_3.py
@@ -0,0 +1,4 @@
+first_name = input("Евгений: ")
+last_name = input("Сенинг: ")
+print("Вас зовут: " + last_name + " " + first_name)
+
