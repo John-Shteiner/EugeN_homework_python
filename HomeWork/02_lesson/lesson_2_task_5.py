1a2be7aaedd1e94990bbdee9c79afab8c4056f5d
Evgenii <jfaust866@gmail.com>
2026-03-06 00:59:26 +0700

    Окончательное исправление заданий 1 и 5

--- HomeWork/02_lesson/lesson_2_task_5.py
+++ HomeWork/02_lesson/lesson_2_task_5.py
@@ -9,18 +9,8 @@ def month_to_season(month):
     elif month in [9, 10, 11]:
         return "Осень"
     else:
-        return "Неверный номер месяца (введите от 1 до 12)"
+        return "Неверный номер месяца"
 
 
-print("Месяц 2:", month_to_season(2))
-print("Месяц 4:", month_to_season(4))
-print("Месяц 7:", month_to_season(7))
-print("Месяц 10:", month_to_season(10))
-print("Месяц 13:", month_to_season(13))
-
-
-
-
-
-
-
+print(month_to_season(2))
+print(month_to_season(5))
