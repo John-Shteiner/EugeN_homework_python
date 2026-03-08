28d3c625404746434280ae6d20e167c256ba8394
Evgenii <jfaust866@gmail.com>
2026-03-08 15:21:55 +0700

    Финальная версия тестов для StringUtils:

--- 04_lesson/test_string_utils.py
+++ 04_lesson/test_string_utils.py
@@ -16,10 +16,10 @@ def test_capitalize(input_str, expected):
 
 # --- 2. TRIM (Удаление пробелов в начале) ---
 @pytest.mark.parametrize("input_str, expected", [
-    ("   skypro", "skypro"),         # Позитивный: пробелы в начале
+    ("   skypro", "skypro"),          # Позитивный: пробелы в начале
     ("  hello world", "hello world"), # Позитивный: пробелы и несколько слов
-    ("", ""),                        # Негативный: пустая строка
-    ("   ", "")                      # Негативный: только пробелы
+    ("", ""),                         # Негативный: пустая строка
+    ("   ", "")                       # Негативный: только пробелы
 ])
 def test_trim(input_str, expected):
     assert utils.trim(input_str) == expected
